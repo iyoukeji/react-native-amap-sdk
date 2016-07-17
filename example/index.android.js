d64d5ac42a38bb4bee8b2d5fc9835f4eb6306312
陈俊生 <junsheng.chen@ggcar.com>
2016-07-17 10:08:33 +0800

    调整example中AppRegistry的方式，统一iOS和Android的代码。

--- example/index.android.js
+++ example/index.android.js
@@ -4,50 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-} from 'react-native';
-import { AMapView, AMapLocationManager } from 'react-native-amap-sdk';
+import {AppRegistry} from 'react-native';
+import Map from './Map';
 
-class Example extends Component {
-  state = {
-    showCallout: false,
-    latitude: 38.89,
-    longitude: 121.58,
-  };
-
-  render() {
-    return (
-      <AMapView style={styles.container} compassEnabled={true}
-        defaultRegion={{"latitude": 38.89, "longitude": 121.58, "latitudeDelta": 0.5, "longitudeDelta": 0.5}}
-        region={{"latitude": this.state.latitude, "longitude": this.state.longitude, "latitudeDelta": 0.5, "longitudeDelta": 0.5}}
-        onRegionChange={(e) => console.log(e)}
-        onUpdateLocation={e => console.log(e)}
-        myLocationEnabled={true}
-        myLocationButtonEnabled={true}>
-        <AMapView.Annotation coordinate={{"latitude": 38.89, "longitude": 121.58}}
-          title="aaa" subtitle="bbbb" enabled={true} selected={true} canShowCallout={true}
-          onSelect={e => console.log(e)}>
-          <AMapView.Callout>
-            <View style={{ width: 100, height: 40, backgroundColor: '#ffffff'}}>
-              <Text>aaaaaabbbbb</Text>
-            </View>
-          </AMapView.Callout>
-        </AMapView.Annotation>
-      </AMapView>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-AppRegistry.registerComponent('Example', () => Example);
+AppRegistry.registerComponent('Example', () => Map);
--- example/index.ios.js
+++ example/index.ios.js
@@ -4,65 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-} from 'react-native';
-import { AMapView, AMapLocationManager } from 'react-native-amap-sdk';
+import {AppRegistry} from 'react-native';
+import Map from './Map';
 
-class Example extends Component {
-  state = {
-    showCallout: false,
-    latitude: 41.0,
-    longitude: 118.0,
-  };
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <AMapView style={styles.container} compassEnabled={true}
-          defaultRegion={{"latitude": 41.0, "longitude": 118.0, "latitudeDelta": 0.5, "longitudeDelta": 0.5}}
-          region={{"latitude": this.state.latitude, "longitude": this.state.longitude, "latitudeDelta": 0.5, "longitudeDelta": 0.5}}
-          myLocationEnabled={true}>
-          <AMapView.Annotation coordinate={{"latitude": 41.0, "longitude": 118.0}}
-            title="aaa" subtitle="bbbb" enabled={true} selected={true} canShowCallout={true}
-            onSelect={e => console.log(e)}>
-            <AMapView.Callout>
-              <View style={{ width: 100, height: 40, backgroundColor: '#ffffff'}}>
-                <Text>aaaaaabbbbb</Text>
-              </View>
-            </AMapView.Callout>
-          </AMapView.Annotation>
-        </AMapView>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <TouchableHighlight onPress={() => {
-            AMapLocationManager.requestCurrentLocation(2, 10, 10)
-              .then(location => console.log('requestCurrentLocation', location))
-              .catch(err => console.log(err));
-            {/*AMapLocationManager.startUpdatingLocation(false, location => {*/}
-              {/*console.log('startUpdatingLocation', location);*/}
-              {/*this.setState({"latitude": location.latitude, "longitude": location.longitude});*/}
-            {/*});*/}
-          }}>
-            <Text>动作</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
-
-AppRegistry.registerComponent('Example', () => Example);
+AppRegistry.registerComponent('Example', () => Map);
